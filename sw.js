97bb33e97e0629e6c4fb916399b8a7a7554fd8b3
yunosensei19-lang <yunosensei19@gmail.com>
2026-09-18 23:11:42 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='myr-game-hub-v16';
+const CACHE='myr-game-hub-v17';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
